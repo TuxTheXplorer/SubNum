334b90eea57cff3e63361933382c45a6085958c5
misilendvai <misilendvai@gmail.com>
2020-10-17 14:33:25 +0100

    First working version noot noot

--- discover.sh
+++ discover.sh
@@ -25,21 +25,31 @@ fi
 
 echo "Network set to: $net"
 
+# Create directory to store results
+dirName=$(echo "$net" | tr "/" "-")
+echo "dirName set to: $dirName"
+mkdir $dirName 2>/dev/null
+cd $dirName
+
 # Run fping on the target network
 echo "Discovering live hosts via fping..."
 fping -a -g $net 2>/dev/null | tee fping.list
 echo "Results written to fping.list."
 
 # Parse fping output. Remove own host
-localhost=$(hostname -I)
+localhost=$(hostname -I | awk '{print $NF}')
 echo "localhost is $localhost"
 
-grep -v $localhost fping.list 2>/dev/null > fping.tmp
+grep -vx $localhost fping.list 2>/dev/null > fping.tmp
 cat fping.tmp > fping.list
 rm fping.tmp
 
 # Run threader3000 on fping results
-./threader6000.py -f fping.list 
+# really hacky way of making sure the results are stored in the right
+# Directory. Will change this later but I'm lazy
+cp ../threader6000.py ./
+cp ../vanillaThreader.py ./
+./threader6000.py -f ./fping.list && rm ./threader6000.py ./vanillaThreader.py && rm -rd ./__pycache__
 
 # Run nmap on open ports (threader3000 output)
-# Thus functionality is already implemented in Threader3000 (Tux version)
+# This functionality is already implemented in Threader6000 (Tux version)
